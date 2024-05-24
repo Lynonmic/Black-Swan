800767f0f55edc3b5cd414efa23232b94ffec126
Lynonmic <nhicora35@gmail.com>
2024-05-25 01:44:14 +0700

    bootstrap

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-const menu = document.querySelector(".navbar_header");
+const menu = document.querySelector(".navbar_links");
+const menu_1 = document.querySelector(".custom_input")
 const menubutton = document.querySelector(".navbar_icons");
 menubutton.addEventListener("click", () => {
+  menu_1.classList.toggle("navbar_open");
   menu.classList.toggle("navbar_open");
   menubutton.classList.toggle("open");
 });
